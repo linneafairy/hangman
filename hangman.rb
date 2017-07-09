0855d02698f07d281da4515e6f96d8a10767b04e
Linnea <linneapari@gmail.com>
2017-07-09 14:16:10 -0700

    fixed bug

--- hangman.rb
+++ hangman.rb
@@ -1,15 +1,18 @@
 words = ["apple", "spaceship", "potato", "onion", "water", "misty", "command", "scrawny", "school", "mend", "window", "carrot", "friend", "university"]
 
+debug = 0
 puts "\n"
 puts "Can You Save Marvin?\n"
-puts "Guess letters of the word. If you guess incorrectly three times, you will lose.\n"
+puts "Guess letters of the word. If you guess incorrectly five times, you will lose.\n"
 myword = words.sample
 length = myword.length
 gameword = myword.split("")
+if debug == 1
+  print "the gameword is #{gameword}"
+end
 
 myarray = ("_") * length
-lives = 3
-i = 0
+lives = 5
 wins = 0
 
 while true
@@ -17,6 +20,9 @@ while true
     puts "Guess a letter."
     myguess = gets.chomp
     indices = gameword.each_with_index.select { |a,i| a == myguess }.map &:last
+    if debug == 1
+      puts indices
+    end
     if indices.length == 0
         print "That letter is not in the word.\n"
         lives -= 1
@@ -26,16 +32,11 @@ while true
         end
     else
         print "Good guess. There are #{indices.length} #{myguess}-s in the word.\n"
-        indices.length
         wins += 1
-        while i < indices.length
-            indices.each {|num| myarray[num] = gameword[num]}
-            i += 1
-        end
+        indices.each {|num| myarray[num] = gameword[num]}
         if wins == gameword.uniq.length
             print "You won.\n"
             break
-        else
         end
     end
 end
